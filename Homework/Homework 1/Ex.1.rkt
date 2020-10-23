11dc4571f79245be40c51dad26a229aa737a9655
vanshianec <ivaniovov@abv.bg>
2020-10-23 11:12:06 +0300

    Added comments

--- Homework/Homework 1/Ex.1.rkt
+++ Homework/Homework 1/Ex.1.rkt
@@ -1,11 +1,12 @@
 #lang racket
-(define (automorphic? n)
+(define (automorphic? n) 
   (define (ends-with? num suffix)
     (cond
-      [(= suffix 0) #t]
-      [(not (= (remainder num 10) (remainder suffix 10))) #f]
-      [else (ends-with? (quotient num 10) (quotient suffix 10))]))
+      [(= suffix 0) #t] ;if the suffix is 0, then num ends with the digits of suffix
+      [(not (= (remainder num 10) (remainder suffix 10))) #f] ;if the last digits of num and suffix are not equal return false 
+      [else (ends-with? (quotient num 10) (quotient suffix 10))])) ;else call the function with the next digits of both numbers
   (ends-with? (* n n) n))
+;NOTE: ends-with? works when num >= suffix and suffix != 0 but since we are receiving natural numbers only, it works fine 
 
 (automorphic? 5) ; -> #t
 (automorphic? 25) ; -> #t
--- Homework/Homework 1/Ex.2.rkt
+++ Homework/Homework 1/Ex.2.rkt
@@ -1,23 +1,28 @@
 #lang racket
 
-(define (calculate k) (+ (* 3 (* k k)) (* 3 k) 1))
-
 (define (is-prime? n)
-  (define (helper index length)
+  (define (loop count length)
      (cond
-       [(> index length) #t]
-       [(= (remainder n index) 0) #f]
-       [else (helper (+ index 1) length)]))
+       [(> count length) #t] ; if the count passes length, the number is prime
+       [(= (remainder n count) 0) #f] ; if the number is divisible by count, then it is not prime
+       [else (loop (+ count 1) length)])) ; else check if the number is divisible by the next count number
   (cond
     [(< n 2) #f]
-    [else (helper 2 (sqrt n))]))
-     
+    [else (loop 2 (sqrt n))])) ; loop from 2 to sqrt(n)
+
+; we can use that num = (k+1)^3 - k^3 = 3*k^2 + 3*k + 1, where k >= 1
+; example : 3^3 - 2^3 = 3*2^2 + 3*2 + 1 = 19
+(define (calculate k) (+ (* 3 (* k k)) (* 3 k) 1))
+ 
 (define (nth-cuban n)
   (define (helper k goal result)
+    ; k -> current number to be calculated
+    ; goal -> keeps track of the count of prime numbers
+    ; result -> keeps the last calculated number by k 
     (cond
-      [(= goal 0) (calculate (- k 1))]
-      [(is-prime? result) (helper (+ k 1) (- goal 1) (calculate (+ k 1)))]
-      [else (helper (+ k 1) goal (calculate (+ k 1)))]))
+      [(= goal 0) (calculate (- k 1))] ; if the goal is zero, then we reached the n-th cuban number
+      [(is-prime? result) (helper (+ k 1) (- goal 1) (calculate (+ k 1)))] ; if the last calculated number is prime, decrease goal and calculate the next number
+      [else (helper (+ k 1) goal (calculate (+ k 1)))])) ; else just calculate the next number
   (helper 0 n 0))
 
 (nth-cuban 1) ; -> 7 (23 - 13)
